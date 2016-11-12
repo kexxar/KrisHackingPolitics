eaa35cdaf02a3fd4332b70a6060c245ee337e36f
Kristina <pupavac.kristina@gmail.com>
2016-11-12 14:59:58 +0100

    Added News List Page and News Page

--- KMMOpenNews/ViewModels/HomePageViewModel.cs
+++ KMMOpenNews/ViewModels/HomePageViewModel.cs
@@ -1,13 +1,56 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace KMMOpenNews
 {
 	public class HomePageViewModel
 	{
 		private readonly HomePage Page;
+		private readonly StackLayout TopNews1;
+		private readonly StackLayout TopNews2;
+		private readonly StackLayout TopNews3;
+
+		public string TopTitle { get; set;}
+		public string TopDescription { get; set; }
+
+		public string TopTitle1 { get; set; }
+		public string TopDescription1 { get; set; }
+
+		public string TopTitle2 { get; set; }
+		public string TopDescription2 { get; set; }
+
+		public string Date1 { get; set;}
+		public string Date2 { get; set; }
+		public string Date3 { get; set; }
+
+		public string Rate1 { get; set; }
+		public string Rate2 { get; set; }
+		public string Rate3 { get; set; }
 
 		public HomePageViewModel(HomePage page)
+		                         //, StackLayout topNews1, StackLayout topNews2, StackLayout topNews3)
 		{
 			Page = page;
+
+			//TopNews1 = topNews1;
+
+			Date1 = DateTime.Today.ToString();
+			Date2 = DateTime.Today.ToString();
+			Date3 = DateTime.Today.ToString();
+
+			Rate1 = "4.7";
+			Rate2 = "4.2";
+			Rate3 = "3.1";
+
+
+			TopTitle = "ewrrretrtrtretvc  dsf";
+			TopDescription = "dsfagasfhhhfj fdgghjgnhdfgg  dfggdfgghs dsfgertdfv edfsgsdf fsadgfdfsgdf dfbd";
+
+			TopTitle1 = "ewrrretrtrtretvc  dsf";
+			TopDescription1 = "dsfagasfhhhfj fdgghjgnhdfgg  dfggdfgghs dsfgertdfv edfsgsdf fsadgfdfsgdf dfbd";
+
+			TopTitle2 = "ewrrretrtrtretvc  dsf";
+			TopDescription2 = "dsfagasfhhhfj fdgghjgnhdfgg  dfggdfgghs dsfgertdfv edfsgsdf fsadgfdfsgdf dfbd";
 		}
 	}
 }
--- KMMOpenNews/ViewModels/NewsListPageViewModel.cs
+++ KMMOpenNews/ViewModels/NewsListPageViewModel.cs
@@ -13,6 +13,18 @@ namespace KMMOpenNews
 			Page = page;
 			NewsList = newsList;
 
+			var customCell = new DataTemplate(typeof(CustomCell));
+
+			NewsList = new ListView {
+				// TODO ItemsSource = "",
+				ItemTemplate = customCell
+			};
+
+			NewsList.ItemSelected += (sender, e) => {
+				//TODO 
+				//Page.Navigation.PushAsync(new NewsPage());
+
+			};
 
 		}
 	}
